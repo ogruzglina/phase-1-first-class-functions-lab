681002450e867ac134f1a76245e2433fea8d8f89
Olha Gruzglina <olga.zhmura@gmail.com>
2022-01-11 15:21:53 -0500

    commit tree 5ef67e47b2212ae56d2c9566c388ed7290e14004

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+const returnFirstTwoDrivers = (arrDrivers) => {
+    const twoDrivers = arrDrivers.slice(0,2);
+    return twoDrivers;
+}
+console.log(returnFirstTwoDrivers(drivers));
+
+const returnLastTwoDrivers = (arrDrivers) => {
+    const lastTwoDrivers = arrDrivers.slice(-2);
+    return lastTwoDrivers;
+}
+console.log(returnLastTwoDrivers(drivers));
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+console.log(selectingDrivers[0](drivers), selectingDrivers[1](drivers));
+
+function createFareMultiplier (multiplier) {
+    return (fare) => fare * multiplier;
+}
+console.log(createFareMultiplier(5)(5));
+
+const fareDoubler = createFareMultiplier(2);
+console.log(fareDoubler(4));
+
+const fareTripler = createFareMultiplier(3);
+console.log(fareTripler(4));
+
+function selectDifferentDrivers(arrayOfDrivers, funcTwoDrivers) {
+    return funcTwoDrivers(arrayOfDrivers);
+}
+console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers));
+console.log(selectDifferentDrivers(drivers, returnLastTwoDrivers));
